02916891345318bd5f1cafff384f370456e80616
Amina Noor <159250991+Amina-codemaker3747@users.noreply.github.com>
2024-02-24 16:55:28 +0500

    Update ziddi.ts

--- 45 Quizes/ziddi.ts
+++ 45 Quizes/ziddi.ts
@@ -677,15 +677,15 @@ if(alien_color === "green") {
  console.log("\tQuestion 28:\n");
  
 let person_age = 17;
-if(person_age > 2) {
+if(person_age < 2) {
   console.log("Baby");
-} else if(person_age > 4) {
+} else if(person_age < 4) {
   console.log("Toddler");
-} else if(person_age > 13) {
+} else if(person_age < 13) {
   console.log("Kid");
-} else if(person_age > 20) {
+} else if(person_age < 20) {
   console.log("Teenager");
-} else if(person_age > 65) {
+} else if(person_age < 65) {
   console.log("Adult");
 } else {
   console.log("Elder");
